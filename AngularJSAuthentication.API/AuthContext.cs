31c913d1d6b0b8069e565592f1064f8cae67b4c2
sirgero <giant_man_j@yahoo.com>
2014-08-17 22:49:15 -0500

    Token Authorization

---
+++ AngularJSAuthentication.API/AuthContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+public class AuthContext : IdentityDbContext<IdentityUser>
+{
+    public AuthContext()
+        : base("AuthContext")
+    {
+
+    }
+}
